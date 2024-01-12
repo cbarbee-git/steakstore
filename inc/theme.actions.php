1ba8c38b7d0115bc29931c7817ab1f7cab15bf58
Chad Barbee <chad_barbee@msn.com>
2024-01-12 13:01:38 -0500

    feat: cleanup unnecessary items from head

--- inc/theme.actions.php
+++ inc/theme.actions.php
@@ -47,4 +47,19 @@ function steakstore_action_enqueue_custom_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'steakstore_action_enqueue_custom_styles', 11 );
 
+function steakstore_remove_json_api() {
+    //clean up head
+    remove_action( 'wp_head', 'feed_links_extra', 3 );
+    remove_action( 'wp_head', 'feed_links', 2 );
+    remove_action( 'wp_head', 'rsd_link' );
+    remove_action( 'wp_head', 'wlwmanifest_link' );
+    remove_action( 'wp_head', 'index_rel_link' );
+    remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+    remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+    remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
+    remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+    remove_action( 'wp_head', 'wp_generator' );
+    remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+}
+add_action( 'after_setup_theme', 'steakstore_remove_json_api' );
 ?>
